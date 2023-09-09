9cca3a1df478c51817f4e1c70c897c66bdcf2e95
Zanna Fitch <zannafitch2004@gmail.com>
2023-09-08 22:30:57 -0500

    Set Up

---
+++ spec/features/markets/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Markets Movie Page', type: :feature do
+  describe 'When I visit /markets' do
+
+    # As a visitor,
+# When I visit '/markets'
+# I see all markets listed with their name, city and state
+# When I click a button to see more info on that market
+# I'm taken to that market's show page '/markets/:id'
+    it 'has a list of all markets with their name, cite, and state' do
+      
+
+
+    end
+
+    it 'has a button to see more info about a market that goes to a market show page' do 
+      
+    end
+  end
+end
