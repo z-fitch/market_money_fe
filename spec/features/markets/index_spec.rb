914306c70b6156ee9a00c280ea8c912c5dc3ac74
Zanna Fitch <zannafitch2004@gmail.com>
2023-09-10 23:30:40 -0500

    Test: add tests for the index page

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Markets Movie Page', type: :feature do
-  describe 'When I visit /markets' do
-
-    # As a visitor,
-# When I visit '/markets'
-# I see all markets listed with their name, city and state
-# When I click a button to see more info on that market
-# I'm taken to that market's show page '/markets/:id'
+  describe 'When I visit /markets', :vcr do
     it 'has a list of all markets with their name, cite, and state' do
+      markets = MarketsFacade.new.all_markets
+      m1 = markets.first
+      m_last = markets.last
       
+      visit markets_path
 
+        expect(page).to have_content(m1.name)
+        expect(page).to have_content(m1.city)
+        expect(page).to have_content(m1.state)
 
-    end
+        expect(page).to have_button("More Info", count: 846)
 
-    it 'has a button to see more info about a market that goes to a market show page' do 
-      
+        expect(page).to have_content(m_last.name)
+        expect(page).to have_content(m_last.city)
+        expect(page).to have_content(m_last.state)
     end
   end
 end
