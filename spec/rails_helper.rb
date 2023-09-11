40bc3c333ecd52cd83d7b3c6b8994d7de81db808
Zanna Fitch <zannafitch2004@gmail.com>
2023-09-10 23:30:13 -0500

    Add webmock/VCR set up

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,3 +72,10 @@ RSpec.configure do |config|
   end
 end
 
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.default_cassette_options = {record: :new_episodes }
+end
+
